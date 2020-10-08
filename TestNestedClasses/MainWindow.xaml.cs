fb0dfdfc7a515d47d52f1db668a1f1b652f302ad
agatino.work@gmail.com <agatino.work@gmail.com>
2020-10-08 08:32:03 +0200

    Add project files.

---
+++ TestNestedClasses/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestNestedClasses
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Test_Click(object sender, RoutedEventArgs e)
+        {
+            Outer.Types type = Outer.Types.One;
+            string stro = Outer.O1;
+            string stri = Outer.InnerA.I1;
+
+        }
+    }
+}
---
+++ TestNestedClasses/Outer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestNestedClasses
+{
+    public static class Outer
+    {
+        public static string O1 => "1";
+        public static class InnerA
+        {
+            public static string I1 => "A1";
+        }
+        public enum Types
+        {
+            None,
+            One,
+            Two
+        }
+    }
+}
